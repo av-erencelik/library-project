3d67808ce3255b976ccdcce56e1642a9b8e4abf4
Mehmet Eren Çelik <erenmemo05@gmail.com>
2022-09-14 17:10:16 +0300

    display function

--- public/js/script.js
+++ public/js/script.js
@@ -1,24 +1,82 @@
-var modal = document.getElementById("modal");
+let myLibrary = [];
+let bookNumber = 0;
+let modal = document.getElementById("modal");
+let btn = document.getElementById("add-book");
+let span = document.getElementsByClassName("close")[0];
+let submit = document.getElementById("submit")
 
-// Get the button that opens the modal
-var btn = document.getElementById("add-book");
+class book {
+    constructor(title,author,pages,isRead) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.isRead = isRead
+    }
+}
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+
+function inputBook(e) {
+    e.preventDefault()
+    let author = document.getElementById("author").value;
+    let title = document.getElementById("title").value;
+    let pages = document.getElementById("pages").value;
+    let isRead = false
+    if(document.getElementById("isRead").checked) {isRead = true}
+    let newBook = new book(author,title,pages,isRead)
+    addBookToLibrary(newBook)
+    clearModalInput()
+    modal.style.display = "none"
+}
+function addBookToLibrary(newBook) {
+    myLibrary.push(newBook)
+    console.log(myLibrary)
+    displayBooks()
+}
+function clearModalInput() {
+    document.getElementById("author").value = ""
+    document.getElementById("title").value = ""
+    document.getElementById("pages").value = ""
+    document.getElementById("isRead").checked = false
+}
+function displayBooks() {
+  let container = document.getElementById("cards")
+  for(let book of myLibrary) {
+    if(!(document.querySelector(`[data-index="${myLibrary.indexOf(book)}"]`)==null)) {
+      continue;
+    }
+    let div = `
+    <div class="card">
+      <h4>${book.title}</h4>
+      <h4>${book.author}</h4>
+      <h4>${book.pages} pages</h4>
+      <div class="buttons">
+          <button class="btn" id="btn" data-index="${myLibrary.indexOf(book)}">Not Read</button>
+          <button class="btn remove">Remove</button>
+      </div>
+    </div>`
+    
+    container.insertAdjacentHTML("beforeend", div)
+    let read = document.querySelector(`[data-index="${myLibrary.indexOf(book)}"]`)
+    console.log(read)
+    if(book.isRead) {
+      read.classList.add("read")
+    }else {
+      read.classList.add("not_read")
+    }
   }
 }
